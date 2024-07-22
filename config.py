4afe45936913384834554d3ba9bfd344fc7eda4a
filipodu982 <filipodu982@gmail.com>
2024-07-22 11:49:32 +0200

    Update .gitignore and stop tracking config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-# Default config.py. Fill it with your personal data and personal boundaries
-from math import radians
-
-MY_ZONE = {"tl_y": 52.24, "tl_x": 20.77, "br_y": 52.04, "br_x": 21.16} # Area over EPWA
-
-TEST_ZONE = {"tl_y": 52.24, "tl_x": 20.77, "br_y": 52.04, "br_x": 21.16}
-
-HOME = [radians(52.163686448190084), radians(21.07648381928902)] # Some random place in Warsaw
